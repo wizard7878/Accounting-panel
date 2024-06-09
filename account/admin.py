c025ad3a45bce5bc6d53d6afd26190a28408c99f
Wizard7878 <peyman934@gmail.com>
2024-06-09 12:50:19 +0330

    add list link display admin

--- account/admin.py
+++ account/admin.py
@@ -7,6 +7,7 @@ class UserAdmin(admin.ModelAdmin):
     model = User
     list_display = ['first_name','last_name','phonenumber', 'storename', 'phoneverified']
     list_filter =  ['phoneverified']
+    list_display_links = ['phonenumber', 'first_name']
 
 
 admin.site.register(User, UserAdmin)
