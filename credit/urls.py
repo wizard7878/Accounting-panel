502d02026998b94b92f62fb2258c66b3c35f1274
Wizard7878 <peyman934@gmail.com>
2024-06-19 14:41:24 +0330

    change html images

--- credit/urls.py
+++ credit/urls.py
@@ -8,7 +8,7 @@ from .views import (
     )
 
 urlpatterns = [
-    path('', index),
+    path('', index, name= 'credit'),
     path('<int:id>/customer/', CustomerCredit.as_view(), name='customer'),
     path('<int:customerId>/customer/<int:creditId>/Long-term-customer-account', CustomerLongCreditView.as_view(), name='long-customer-account'),
     path('<int:customerId>/customer/<int:creditId>/grami-customer-account', CustomerGramiCreditView.as_view(), name='grami-customer-account'),
